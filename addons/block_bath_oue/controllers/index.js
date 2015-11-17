f9dece096d97dc8a4fe29095294f0715cca1a482
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2015-11-17 22:22:00 +0000

    Added block_bath_oue initial files

---
+++ addons/block_bath_oue/controllers/index.js
@@ -0,0 +1,24 @@
+angular.module('mm.addons.bath_oue')
+/**
+ * OUE controller.
+ *
+ * @module mm.addons.bath_oue
+ * @ngdoc controller
+ * @name OUEController
+ */
+.controller('OUEController',function($scope,$mmSite){
+        var currentUserId = $mmSite.getUserId();
+
+        function fetchOUE(currentUserId){
+
+            //Display progress bar
+
+            //Display complete now button
+
+            //Department response
+
+            //Better at bath logo
+
+            //Prize draw promotion
+        }
+});
